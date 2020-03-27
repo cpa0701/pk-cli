8bd943b3f997486e534f6259542c59686fa12e7f
cpa0701 <121046856@qq.com>
2020-03-28 00:10:51 +0800

    改变为pk-cli执行

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-#!/usr
-console.log("this is pk-cli");
+#! node
+const yeoman = require("yeoman-environment");
+const env = yeoman.createEnv();
+env.register(require.resolve("../generators/app"), "pk-cli");
+env.run("pk-cli");
